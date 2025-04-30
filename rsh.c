22942a70664e5561894bd442343b4e8ae0e7c544
Wesley-Montgomery-Mines <wesley_montgomery@mines.edu>
2025-04-30 13:25:46 -0600

    Update rsh.c

--- rsh.c
+++ rsh.c
@@ -8,26 +8,25 @@
 #include <pthread.h>
 #include <signal.h>
 
-// This is the total number of allowed commands including sendmsg
 #define N 13
 
-extern char **environ; // This is for posix_spawn
-char uName[20];		   // This stores the current user's name
+extern char **environ;
+char uName[20]; // This is the global variable to store username
 
-// This is the list of allowed commands
+// These are the allowed commands
 char *allowed[N] = {
 	"cp", "touch", "mkdir", "ls", "pwd", "cat", "grep", "chmod",
 	"diff", "cd", "exit", "help", "sendmsg"};
 
-// This is for the structure for inter-process messages
+// This is the message structure exchanged via FIFOs
 struct message
 {
-	char source[50]; // This is the sender's username
-	char target[50]; // This is the recipient's username
-	char msg[200];	 // This is the message body
+	char source[50]; // This is the sender username
+	char target[50]; // This is the recipient username
+	char msg[200];	 // This is the message content
 };
 
-// This is for signal handler for CTRL + C or termination
+// This will signal handler to clean up on termination
 void terminate(int sig)
 {
 	printf("Exiting....\n");
@@ -35,52 +34,60 @@ void terminate(int sig)
 	exit(0);
 }
 
-// This sends a message to the server FIFO for forwarding to the target
+// The is the function to send a message from this user to another user
 void sendmsg(char *user, char *target, char *msg)
 {
-	struct message msgStructure;
-	strcpy(msgStructure.target, target);
-	strcpy(msgStructure.msg, msg);
-	strcpy(msgStructure.source, user);
+	struct message m;
+	strcpy(m.source, user);
+	strcpy(m.target, target);
+	strcpy(m.msg, msg);
 
-	int writer = open("serverFIFO", O_WRONLY);
-	if (writer != -1)
+	int fidscrpt = open("serverFIFO", O_WRONLY);
+	if (fidscrpt < 0)
 	{
-		write(writer, &msgStructure, sizeof(struct message));
-		close(writer);
+		perror("sendmsg: open");
+		return;
 	}
+
+	write(fidscrpt, &m, sizeof(m));
+	close(fidscrpt);
 }
 
-// This is the thread that listens to this user's FIFO and prints incoming messages
+// This is the thread function to continuously listen for messages sent to this user
 void *messageListener(void *arg)
 {
-	int userFd;
-	struct message userRead;
+	char fifoName[50];
+	snprintf(fifoName, sizeof(fifoName), "%s", uName);
+
+	int fidscrpt;
+	struct message m;
 
 	signal(SIGPIPE, SIG_IGN);
 	signal(SIGINT, terminate);
 
 	while (1)
 	{
-		userFd = open(uName, O_RDONLY);
-		if (userFd == -1)
+		fidscrpt = open(fifoName, O_RDONLY);
+		if (fidscrpt < 0)
 		{
-			perror("Failed to open user FIFO");
+			perror("messageListener: open");
 			sleep(1);
 			continue;
 		}
 
-		if (read(userFd, &userRead, sizeof(struct message)) > 0)
+		if (read(fidscrpt, &m, sizeof(struct message)) > 0)
 		{
-			printf("Incoming message from %s: %s\n", userRead.source, userRead.msg);
+			printf("Incoming message from %s: %s\n", m.source, m.msg);
 			fflush(stdout);
 		}
-		close(userFd);
+
+		close(fidscrpt);
 	}
+
 	pthread_exit(NULL);
 }
 
-// This will return 1 if the command is allowed or else 0
+// This checks if a given command is allowed
 int isAllowed(const char *cmd)
 {
 	for (int i = 0; i < N; i++)
@@ -91,7 +98,7 @@ int isAllowed(const char *cmd)
 	return 0;
 }
 
-// This is the main loop for rsh shell
+// This is the main shell interface logic
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -100,27 +107,28 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	// This will store the username
 	strcpy(uName, argv[1]);
 	signal(SIGINT, terminate);
 
-	// This will start the listener thread
+	// This will create the message listener thread
 	pthread_t msgThreadId;
 	pthread_create(&msgThreadId, NULL, messageListener, NULL);
 
+	char line[256];
 	while (1)
 	{
-		fprintf(stderr, "rsh>");
-		char line[256], line2[256], cmd[256];
+		fprintf(stderr, "rsh> ");
 
 		if (fgets(line, sizeof(line), stdin) == NULL)
 			continue;
 		if (strcmp(line, "\n") == 0)
 			continue;
+		line[strcspn(line, "\n")] = '\0';
 
-		line[strlen(line) - 1] = '\0';
-		strcpy(line2, line);
-		strcpy(cmd, strtok(line, " "));
+		// This will parse command and arguments
+		char *cmd = strtok(line, " ");
+		if (!cmd)
+			continue;
 
 		if (!isAllowed(cmd))
 		{
@@ -128,42 +136,24 @@ int main(int argc, char **argv)
 			continue;
 		}
 
-		// This will handle the special sendmsg command
+		// This will handle the built in sendmsg command
 		if (strcmp(cmd, "sendmsg") == 0)
 		{
-			char *args[20] = {0};
-			char *token = strtok(line2, " ");
-			int argCount = 0;
-			while (token != NULL && argCount < 19)
-			{
-				args[argCount++] = token;
-				token = strtok(NULL, " ");
-			}
-			args[argCount] = NULL;
+			char *target = strtok(NULL, " ");
+			char *msgBody = strtok(NULL, "");
 
-			if (!args[1])
+			if (!target)
 			{
 				printf("sendmsg: you have to specify a target\n");
 				continue;
 			}
-			if (!args[2])
+			if (!msgBody)
 			{
 				printf("sendmsg: you have to enter a message\n");
 				continue;
 			}
 
-			// This will reconstruct message string from args[2..]
-			char *message = (char *)malloc(256);
-			message[0] = '\0';
-			for (int i = 2; i < argCount; i++)
-			{
-				strcat(message, args[i]);
-				if (i < argCount - 1)
-					strcat(message, " ");
-			}
-
-			sendmsg(argv[1], args[1], message);
-			free(message);
+			sendmsg(uName, target, msgBody);
 			continue;
 		}
 
@@ -171,60 +161,56 @@ int main(int argc, char **argv)
 		if (strcmp(cmd, "exit") == 0)
 			break;
 
-		// This will handle the cd command internally
+		// This will handle the cd command
 		if (strcmp(cmd, "cd") == 0)
 		{
-			char *targetDir = strtok(NULL, " ");
-			if (strtok(NULL, " ") != NULL)
-				printf("-rsh: cd: too many arguments\n");
+			char *dir = strtok(NULL, " ");
+			if (strtok(NULL, " "))
+			{
+				printf("cd: too many arguments\n");
+			}
 			else
-				chdir(targetDir);
+			{
+				chdir(dir);
+			}
 			continue;
 		}
 
 		// This will handle the help command
 		if (strcmp(cmd, "help") == 0)
 		{
-			printf("The allowed commands are:\n");
+			printf("Allowed commands:\n");
 			for (int i = 0; i < N; i++)
-				printf("%d: %s\n", i + 1, allowed[i]);
+			{
+				printf("%s\n", allowed[i]);
+			}
 			continue;
 		}
 
-		// This will handle allowed external commands using posix_spawn
-		char *path = strdup(cmd);
-		char **cargv = malloc(sizeof(char *) * 2);
-		cargv[0] = strdup(cmd);
-
-		int n = 1;
-		char *arg = strtok(line2, " ");
-		arg = strtok(NULL, " ");
-		while (arg != NULL)
+		// This will spawn external allowed command
+		char *args[20];
+		args[0] = cmd;
+		int argCount = 1;
+		char *token;
+		while ((token = strtok(NULL, " ")) != NULL)
 		{
-			cargv = realloc(cargv, sizeof(char *) * (n + 2));
-			cargv[n++] = strdup(arg);
-			arg = strtok(NULL, " ");
+			args[argCount++] = token;
 		}
-		cargv[n] = NULL;
+		args[argCount] = NULL;
 
 		posix_spawnattr_t attr;
 		posix_spawnattr_init(&attr);
 		pid_t pid;
 
-		if (posix_spawnp(&pid, path, NULL, &attr, cargv, environ) != 0)
+		if (posix_spawnp(&pid, cmd, NULL, &attr, args, environ) != 0)
 		{
-			perror("spawn failed");
-			exit(EXIT_FAILURE);
+			perror("posix_spawnp failed");
+			continue;
 		}
 
 		int status;
 		waitpid(pid, &status, 0);
 		posix_spawnattr_destroy(&attr);
-
-		free(path);
-		for (int i = 0; i < n; i++)
-			free(cargv[i]);
-		free(cargv);
 	}
 
 	return 0;
